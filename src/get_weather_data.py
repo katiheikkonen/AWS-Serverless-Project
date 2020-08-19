8570bfdfb9f697ce492c62d6802d81782367af7a
MitjaHaimila <mitja.haimila@gmail.com>
2020-08-19 15:56:50 +0300

    get weather ja get covid toimii

--- src/get_weather_data.py
+++ src/get_weather_data.py
@@ -1,22 +1,25 @@
+import boto3
 import requests
 import json
-import os
+
+ssm = boto3.client('ssm', region_name='ap-northeast-1')
+
 
 def get_weather_data(event, context):
+    parameter = ssm.get_parameter(Name='WeatherAPI', WithDecryption=True)
+    apikey = parameter['Parameter']['Value']
 
-    usercity = json.loads(event['Item']['city'])
-    my_api_key = os.environ['APIKEY']
-    weather_data = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?units=metric&q={usercity}&appid={my_api_key}')
+    usercity = event['Item']['city']
+    weather_data = requests.get(
+        f'https://api.openweathermap.org/data/2.5/forecast?units=metric&q={usercity}&appid={apikey}')
     city_data = weather_data.json()
     data = {
-        "weather": city_data['list'][0]['weather'][0]['main'], #  weather (Clear, Clouds, Rain)
-        "weather_decription": city_data['list'][0]['weather'][0]['description'], #  more precise description on weather
-        "temperature": city_data['list'][0]['main']['temp'], #  current temperature
-        "feels_like": city_data['list'][0]['main']['feels_like'] # what temperature feels like
+        "weather": city_data['list'][0]['weather'][0]['main'],  # weather (Clear, Clouds, Rain)
+        "weather_decription": city_data['list'][0]['weather'][0]['description'],  # more precise description on weather
+        "temperature": city_data['list'][0]['main']['temp'],  # current temperature
+        "feels_like": city_data['list'][0]['main']['feels_like']  # what temperature feels like
     }
-    print(json.dumps(data))
-    return {
-         "statusCode": 200,
-         'body': json.dumps(data)
-    }
+
+    return data
+
 
